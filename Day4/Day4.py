601b29a945198fb97d6e5263db0871b5276935e1
HDee-hd <hdairo199@gmail.com>
2024-01-15 23:13:58 +0100

    Fixes

---
+++ Day4/Day4.py
@@ -0,0 +1,4 @@
+
+challenge = 'thirty\tdays\tof\tpython'
+print(challenge.expandtabs())   # 'thirty  days    of      python'
+print(challenge.expandtabs(10)) # 'thirty    days      of        python'
