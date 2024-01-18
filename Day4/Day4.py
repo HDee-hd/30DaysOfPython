f5847df9cbf8fc6346fd88243022ec2fa54a1f55
HDee-hd <hdairo199@gmail.com>
2024-01-18 21:04:36 +0100

    New

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -8,3 +8,21 @@ thir = "Of"
 four = "Python"
 space = " "
 print(first + space + sec + space + thir + space + four)
+
+thi = "Coding"
+fou = "For"
+na = "All"
+company = thi + space + fou + space + na
+print(len(company))
+print(company)
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+result = company[0:6]
+print(result)
+nResult = (company.find("coding"))
+print(nResult)
+print(company.replace("Coding", "Python"))
+print(company.split(" "))
