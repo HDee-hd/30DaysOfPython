96b7daab73c2d6d30d1afcdf91912f02271f3e6d
HDee-hd <hdairo199@gmail.com>
2024-01-18 20:32:52 +0100

    Updated

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -2,3 +2,9 @@
 challenge = 'thirty\tdays\tof\tpython'
 print(challenge.expandtabs())   # 'thirty  days    of      python'
 print(challenge.expandtabs(10)) # 'thirty    days      of        python'
+first = "Thirty"
+sec = "Days"
+thir = "Of"
+four = "Python"
+space = " "
+print(first + space + sec + space + thir + space + four)
