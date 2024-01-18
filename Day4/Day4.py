6a3ed8d05e011fc8d55faa3f5bda23cea7662f1b
HDee-hd <hdairo199@gmail.com>
2024-01-18 23:02:34 +0100

    Day 4 completed

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -40,3 +40,43 @@ print(company.rfind("l"))
 word = "You cannot end a sentence with because because because is a conjunction"
 print(word.index("because"))
 print(word.rindex("because"))
+print(word[31:54])
+print(word.find("because"))
+print(company.startswith("Coding"))
+print(company.endswith("Coding"))
+newCompany = "'   Coding For All      ' "
+print(newCompany.strip("  ' "))
+v = "30DaysOfPython"
+u = "thirty_days_of_python"
+print(v.isidentifier())
+print(u.isidentifier())
+# The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+lib = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print("# ".join(lib))
+
+print("I am enjoying this challenge.\nI just wonder what is next.")
+# Name      Age     Country   City
+# Asabeneh  250     Finland   Helsinki
+print("Name\tAge\tCountry\tCity")
+print("Hassan\t25\tNigeria\tIbadan")
+
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area}")
+
+d = 8
+f = 6
+ans = d + f
+anss = d - f
+ansm = d * f
+ansd = d / f
+ansmo = d % f
+ansf = d // f
+anse = 8 ** 6
+print(f"{d} + {f} is {ans}")
+print(f"{d} - {f} is {anss}")
+print(f"{d} * {f} is {ansm}")
+print(f"{d} / {f} is {ansd}")
+print(f"{d} % {f} is {ansmo}")
+print(f"{d} // {f} is {ansf}")
+print(f"{d} ** {f} is {anse}")
