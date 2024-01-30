6010fac5546bd05ee2fa824a6d5e710b2e76af75
HDee-hd <hdairo199@gmail.com>
2024-01-30 03:31:06 +0100

    Added new

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -13,14 +13,17 @@ namess = names[0]
 print(namess)
 
 
+
 # Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
-
+nameM = ["Hassan", 12, 6.0, "Married", "52, Bakery road."]
 # Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
-
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 # Print the list using print()
-
+print(it_companies)
 # Print the number of companies in the list
-
+print("Number of companies: ",len(it_companies))
 # Print the first, middle and last company
-
+print(it_companies[0:3:6])
 # Print the list after modifying one of the companies
+it_companies[1] = "Hasdeen"
+print(it_companies)
