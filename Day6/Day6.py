a99848eddbbe24ffdbb9895cd97cdbd8626f3245
HDee-hd <hdairo199@gmail.com>
2024-02-01 05:13:44 +0100

    Added new

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,3 +1,15 @@
 # syntax
-tpl = ('item1', 'item2', 'item3')
-print(len(tpl))
+# Create an empty tuple
+emt = ()
+# Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+bro = ('Hassan', 'Hussayn', 'Abdullah')
+sis = ("Aminat", "Aisha", "Aliya")
+sibl = bro + sis
+# Join brothers and sisters tuples and assign it to siblings
+# How many siblings do you have?
+print("I have ", len(sibl), " siblings.")
+# Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+fam = list(sibl)
+fam.append("Dad")
+fam.append("Mom")
+print(fam)
