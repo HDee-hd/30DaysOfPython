1ace07b98a922a7232a5453aa7725fcbbfef8bf8
HDee-hd <hdairo199@gmail.com>
2024-02-01 04:53:26 +0100

    Added new

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -80,3 +80,18 @@ print(full_stack)
 full_stack.append("Python")
 full_stack.append("SQL")
 print(full_stack)
+
+# The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# Sort the list and find the min and max age
+ages.sort()
+min_age = ages[0]
+last_index = len(ages) - 1
+max_age = ages[last_index]
+print(ages, min_age, max_age)
+
+# Add the min age and the max age again to the list
+# Find the median age (one middle item or two middle items divided by two)
+# Find the average age (sum of all items divided by their number )
+# Find the range of the ages (max minus min)
+# Compare the value of (min - average) and (max - average), use abs() method
