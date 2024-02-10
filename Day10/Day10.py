76cd8bc254cb2303500963297015721534736ef6
HDee-hd <hdairo199@gmail.com>
2024-02-10 11:51:56 +0100

    Fixes

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -2,5 +2,48 @@ count = 0
 while count < 5:
     print(count)
     count = count + 1
-    if count == 3:
-        break
+# else:
+#     print(count)
+
+# count = 0
+# while count < 5:
+#     if count == 3:
+#         count = count + 1
+#         continue
+#     print(count)
+#     count = count + 1
+
+    language = 'Python'
+for letter in language:
+    print(letter)
+
+
+for i in range(len(language)):
+    print(language[i])
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value) # this way we get both keys and values printed out
+
+
+    numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
+print('outside the loop')
